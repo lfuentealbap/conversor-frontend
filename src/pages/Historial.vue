2c7274965042d0a12e79edfbae88ef5b2ba9ab2d
Luis Fuentealba Pérez <lfuentealbap@outlook.cl>
2023-10-17 14:43:16 -0300

    implementación de Exportar a Excel

--- src/pages/Historial.vue
+++ src/pages/Historial.vue
@@ -1,6 +1,19 @@
 <template>
   <Navbar />
   <h1 class="mb-5 mt-2 text-center">Historial de conversiones</h1>
+  <div class="container-fluid mb-5">
+    <button @click="exportarExcel" class="btn btn-success">
+       <icon><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-file-type-xls" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+       <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+       <path d="M14 3v4a1 1 0 0 0 1 1h4" />
+       <path d="M5 12v-7a2 2 0 0 1 2 -2h7l5 5v4" />
+       <path d="M4 15l4 6" />
+       <path d="M4 21l4 -6" />
+       <path d="M17 20.25c0 .414 .336 .75 .75 .75h1.25a1 1 0 0 0 1 -1v-1a1 1 0 0 0 -1 -1h-1a1 1 0 0 1 -1 -1v-1a1 1 0 0 1 1 -1h1.25a.75 .75 0 0 1 .75 .75" />
+       <path d="M11 15v6h3" />
+      </svg></icon> Exportar a Excel
+    </button>
+  </div>
   <section class="container-fluid">
     <div class="table-responsive">
       <table class="table table-bordered">
@@ -34,6 +47,7 @@ import { ref, onMounted } from 'vue';
 import Navbar from '../components/Navbar.vue';
 import { format } from 'date-fns';
 import Axios from 'axios';
+import ExcelJS from 'exceljs';
 
 
 // Arreglo de conversiones reactiva
@@ -48,6 +62,48 @@ const obtenerHistorial = async () => {
     console.error(error);
   }
 };
+
+
+const exportarExcel = () => {
+  const workbook = new ExcelJS.Workbook();
+  const worksheet = workbook.addWorksheet('Historial de Conversiones');
+  
+  // Agregar encabezados de columna
+  const headers = [
+    'Fecha Hora Actividad',
+    'Usuario',
+    'Monto Origen',
+    'Fecha Conversión',
+    'Valor de la Moneda',
+    'Resultado Conversión'
+  ];
+  worksheet.addRow(headers);
+  
+  // Agregar filas de datos
+  conversiones.value.forEach((conversion) => {
+    const rowData = [
+      format(new Date(conversion.fechaHoraActividad), 'dd-MM-yyyy HH:mm') + ' hrs',
+      conversion.usuario.nombre,
+      conversion.montoOrigen + ' UF',
+      format(new Date(conversion.fechaConversion), 'dd-MM-yyyy'),
+      '$' + conversion.valorMoneda + ' CLP',
+      '$' + conversion.montoConversion + ' CLP'
+    ];
+    worksheet.addRow(rowData);
+  });
+  
+  // Escribir el archivo de Excel
+  workbook.xlsx.writeBuffer().then((buffer) => {
+    const blob = new Blob([buffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'historial_conversiones.xlsx';
+    a.click();
+    URL.revokeObjectURL(url);
+  });
+};
+
 
 // Hook de montaje
 onMounted(() => {
